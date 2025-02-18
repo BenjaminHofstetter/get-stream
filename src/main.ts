85300db8f42ad2f7b773fbe77ac92f6bedef70e9
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2025-02-18 08:09:56 +0100

    add get-stream

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import getStream from 'get-stream';
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+window.setTimeout(async () => {
+
+
+  const { body: readableStream } = await fetch('http://localhost:4200');
+  if (!readableStream) {
+    throw new Error('Failed to fetch');
+  }
+  console.log(await getStream(readableStream));
+}, 0);
